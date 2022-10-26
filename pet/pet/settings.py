0696ca49e42e9817582e7d2945aa26f3ed80b29f
takayuu1999 <ykh2135246@stu.o-hara.ac.jp>
2022-10-26 11:42:56 +0900

    settings.pyの編集

--- pet/pet/settings.py
+++ pet/pet/settings.py
@@ -68,6 +68,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': '',
+        'POST': '',
     }
 }
 
