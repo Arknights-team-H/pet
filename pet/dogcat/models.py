ea5f0ae1eee491e2388ff32ed1ed6683321faaef
patoranti <famima.tomorrow@gmail.com>
2022-10-28 12:29:56 +0900

    修正

--- pet/dogcat/models.py
+++ pet/dogcat/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 class Dogcat(models.Model):
-    number = models.CharField(max_length=255, verbose_name='個体番号：', blank=True, null=True)
-    data = models.DateField('日付：', blank=True, null=True)
-    vaccination = models.CharField(max_length=255, verbose_name='接種ワクチン：')
+    number = models.IntegerField(max_length=255, verbose_name='個体番号', blank=True, null=True)
+    data = models.DateField('日付', blank=True, null=True)
+    vaccination = models.CharField(max_length=255, verbose_name='接種ワクチン')
     hospitalid = models.CharField(max_length=255, verbose_name='病院ID', blank=True, null=True)
 
     class Meta:
