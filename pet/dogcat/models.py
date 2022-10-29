091248cc34ceca014d20526588d02b71b25c69bf
takayuu1999 <ykh2135246@stu.o-hara.ac.jp>
2022-10-30 00:25:07 +0900

    model　一部追加

--- pet/dogcat/models.py
+++ pet/dogcat/models.py
@@ -14,3 +14,14 @@ class MasterVaccination(models.Model):
 
     class Meta:
         verbose_name_plural = 'MasterVaccination'
+
+class User(models.Model):
+    user_id = models.CharField(max_length=10, verbose_name='ユーザID')
+    mc_number = models.CharField(max_length=15, verbose_name='個体番号')
+    user_password = models.CharField(max_length=16, verbose_name='パスワード')
+    user_birthday = models.DateField('生年月日')
+    createdata = models.DateField('作成日付', blank=True, null=True)
+    updatedata = models.DateField('最終更新日', blank=True, null=True)
+
+    class Meta:
+        verbose_name_plural = 'User'
