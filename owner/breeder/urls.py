565966577c25cb0d9073d444d65ec748542534cb
takayuu1999 <ykh2135246@stu.o-hara.ac.jp>
2022-11-17 15:32:20 +0900

    ownerアプリケーション作成

--- owner/breeder/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-app_name = 'breeder'
-urlpatterns = [
-    path('', views.NotHomeView.as_view(), name="nothome"),
-    path('index', views.IndexView.as_view(), name="index"),
-]
--- owner/breeder/views.py
+++
@@ -1,7 +0,0 @@
-from django.views import generic
-
-class NotHomeView(generic.TemplateView):
-    template_name = "nothome.html"
-
-class IndexView(generic.TemplateView):
-    template_name = "index.html"
