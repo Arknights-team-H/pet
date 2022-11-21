d2c2a4047d8fc3f8866dc35709c6db23bbc10022
PC406-125\ict <ykh2135025@stu.o-hara.ac.jp>
2022-11-21 11:11:49 +0900

    owner ok

--- owner/views.py
+++ owner/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+class NotHomeView(generic.TemplateView):
+    template_name = "nothome.html"
+
+class UserindexView(generic.TemplateView):
+    template_name = "userindex.html"
+
+class DrugView(generic.TemplateView):
+    template_name = "drug.html"
+
+class Drug_createView(generic.TemplateView):
+    template_name = "drug_create.html"
+
+class StoreView(generic.TemplateView):
+    template_name = "store.html"
+
+class UserloginView(generic.TemplateView):
+    template_name = "userlogin.html"
+
+class UsersignupView(generic.TemplateView):
+    template_name = "usersignup.html"
+
+class UserlogoutView(generic.TemplateView):
+    template_name = "userlogout.html"
