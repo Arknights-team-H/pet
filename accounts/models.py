e2543fe84e2614e284e78011fd7ae5309fd6b1cb
patoranti <famima.tomorrow@gmail.com>
2023-02-06 16:26:23 +0900

    inoue

--- accounts/models.py
+++ accounts/models.py
@@ -8,6 +8,7 @@ class MyUser(AbstractUser):
         verbose_name = 'ユーザ'
         verbose_name_plural = 'ユーザ'
 
+    email = models.EmailField(verbose_name='メールアドレス')
     mc_number = models.CharField(max_length=15, verbose_name='個体番号(15桁)')
     user_birthday = models.DateField(verbose_name='ユーザの生年月日')
     pet_id = models.CharField(max_length=11, verbose_name='ペットID')
