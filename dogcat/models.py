d766adca13b0c0c772b783987e2fbb345910fbdd
takayuu1999 <ykh2135246@stu.o-hara.ac.jp>
2023-02-06 10:15:14 +0900

    GOAT阪本

--- dogcat/models.py
+++ dogcat/models.py
@@ -3,11 +3,24 @@ from django.utils import timezone
 
 class MasterVaccination(models.Model): # 予防接種種類のマスタテーブル
     vaccination_type = models.CharField(max_length=40, verbose_name='ワクチン種類')
-
     def __str__(self):
         return self.vaccination_type
     class Meta:
         verbose_name_plural = 'MasterVaccination'
+
+class MasterSpecies(models.Model): # 種別のマスタテーブル
+    species = models.CharField(max_length=1, verbose_name='種別')
+    def __str__(self):
+        return self.species
+    class Meta:
+        verbose_name_plural = 'MasterSpecies'
+
+class MasterGender(models.Model): # 性別のマスタテーブル
+    gender = models.CharField(max_length=2, verbose_name='性別')
+    def __str__(self):
+        return self.gender
+    class Meta:
+        verbose_name_plural = 'MasterGender'
 
 
 class Vaccination(models.Model): # 病院側の予防接種情報登録テーブル
@@ -18,26 +31,12 @@ class Vaccination(models.Model): # 病院側の予防接種情報登録テーブ
     owner_name = models.CharField(max_length=100, verbose_name='飼い主名')
     owner_address = models.CharField(max_length=255, verbose_name='住所')
     pet_name = models.CharField(max_length=100, verbose_name='ペット名')
-    species = models.CharField(max_length=100, verbose_name='種別')
-    gender = models.CharField(max_length=2, verbose_name='性別')
+    species = models.ForeignKey(MasterSpecies, on_delete=models.DO_NOTHING, verbose_name='種別')
+    gender = models.ForeignKey(MasterGender, on_delete=models.DO_NOTHING, verbose_name='性別')
 
     class Meta:
         verbose_name_plural = 'Vaccination'
 
-
-# class MasterUser(models.Model): # 一般ユーザ側アプリのユーザ情報テーブル
-#     user_id = models.CharField(max_length=10, verbose_name='ユーザID')
-#     mc_number = models.CharField(max_length=15, verbose_name='個体番号')
-#     user_password = models.CharField(max_length=16, verbose_name='パスワード')
-#     user_birthday = models.DateField('ユーザの生年月日')
-#     pet_id = models.CharField(max_length=11, verbose_name='ペットID')
-#     pet_name = models.CharField(max_length=40, verbose_name='名前')
-#     pet_birthday = models.DateField('ペットの生年月日')
-#     createdate = models.DateField('作成日付', blank=True, null=True)
-#     updatedate = models.DateField('最終更新日', blank=True, null=True)
-#
-#     class Meta:
-#         verbose_name_plural = 'MasterUser'
 
 class MasterHospital(models.Model): # 病院情報のマスタテーブル
     hospital_name = models.CharField(max_length=40, verbose_name='病院名')
@@ -63,7 +62,6 @@ class MasterHospitalUser(models.Model): # 病院側の病院情報テーブル
     hospital_id = models.CharField(max_length=7, verbose_name='病院ID')
     password = models.CharField(max_length=256, verbose_name='パスワード')
 
-
     class Meta:
         verbose_name_plural = 'Hospital'
 
@@ -86,24 +84,6 @@ class Medicine(models.Model): # 一般ユーザ側のお薬情報テーブル
     class Meta:
         verbose_name_plural = 'Medicine'
 
-class LoginHistory(models.Model): # ログイン判別テーブル
-
-    login = models.DateField('ログイン日', blank=True, null=True)
-    login_limit = models.DateField('ログインの有効期限', blank=True, null=True)
-    logout_flag = models.CharField(max_length=1, verbose_name='ログアウトフラグ')
-    distinction_flag = models.CharField(max_length=1, verbose_name='一般、病院判別フラグ')
-
-    class Meta:
-        verbose_name_plural = 'LoginHistory'
-
-class AlarmHistory(models.Model): # 一般ユーザ側のアラートテーブル
-
-    issue = models.DateField('発行日', blank=True, null=True)
-    issue_limit = models.DateField('発行期限', blank=True, null=True)
-    alarm_content = models.CharField(max_length=40, verbose_name='アラート内容')
-
-    class Meta:
-        verbose_name_plural = 'AlarmHistory'
 
 class QRcodeHistory(models.Model): # 一般ユーザ側のQRコード発行テーブル
 
