00b8762683da67639a1e1b014ddd5f1d8f18eb0f
patoranti <famima.tomorrow@gmail.com>
2023-02-07 16:37:43 +0900

    inoue

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.core.validators import MinLengthValidator
+from django.core.validators import RegexValidator
 
 class MyUser(AbstractUser):
     """拡張ユーザーモデル"""
@@ -9,7 +11,10 @@ class MyUser(AbstractUser):
         verbose_name_plural = 'ユーザ'
 
     email = models.EmailField(verbose_name='メールアドレス')
-    mc_number = models.CharField(max_length=15, verbose_name='個体番号(15桁)')
+    mc_number = models.CharField(max_length=15, verbose_name='個体番号',
+                                 validators=[MinLengthValidator(15, '個体番号は15文字です。'),
+                                             RegexValidator(r'^[0-9]*$', '数字のみを入力してください。')],
+                                 )
     user_birthday = models.DateField(verbose_name='ユーザの生年月日')
     pet_id = models.CharField(max_length=11, verbose_name='ペットID')
     pet_name = models.CharField(max_length=40, verbose_name='ペットの名前')
