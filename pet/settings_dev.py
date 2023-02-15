25c15da6a516cf61e646cc021e7d320599b7bfbd
takayuu1999 <ykh2135246@stu.o-hara.ac.jp>
2023-02-15 17:16:49 +0900

    css 修正 駄作

--- pet/settings_dev.py
+++ pet/settings_dev.py
@@ -49,4 +49,12 @@ LOGGING = {
     }
 }
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# メール送信
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_USE_TLS = True
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_HOST_USER = 'ykh2135246@gmail.com'
+# EMAIL_HOST_PASSWORD = 'duegjxhkwvhrlkif'
+# EMAIL_PORT =587
