8c54bd570a27c4c01ff5c325281df81374cf3902
patoranti <famima.tomorrow@gmail.com>
2023-02-15 17:15:13 +0900

    inoue

--- pet/settings_common.py
+++ pet/settings_common.py
@@ -66,7 +66,7 @@ WSGI_APPLICATION = 'pet.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'dogcat',
+        'NAME': 'UTTA',
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASSWORD'),
         'HOST': '',
